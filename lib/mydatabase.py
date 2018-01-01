944735d2b0116e7bb6bc4120759ea8672af48a31
jaswinder Singh <jaswinder.davc@gmail.com>
2018-01-02 03:46:12 +0530

    For only first commit, out of list of 30 repo commits, Properly working insertRepoCommits() method added to mydatabase, but yet adding only one record to commit table. Also .gitignore added for __pycache__

--- lib/mydatabase.py
+++ lib/mydatabase.py
@@ -16,13 +16,13 @@ def insertRepo(dbConfig, repoData):
         myCursor = cnx.cursor()  # Cursor Creation
         # keys for fetchting only required keys
 
-        ghAttribs = ['id', 'name', 'full_name', 'user_id', 'fork', 'created_at', 'updated_at', 'pushed_at', 'homepage', 'size', 'stargazers_count',
-                     'subscribers_count', 'forks', 'language', 'has_issues', 'has_pages', 'has_wiki', 'archived', 'open_issues', 'license', 'network_count']
-
         repoData['user_id'] = repoData['owner']['id']
 
         if repoData['license'] != None:
             repoData['license'] = repoData['license']['key']
+
+        ghAttribs = ['id', 'name', 'full_name', 'user_id', 'fork', 'created_at', 'updated_at', 'pushed_at', 'homepage', 'size', 'stargazers_count',
+                     'subscribers_count', 'forks', 'language', 'has_issues', 'has_pages', 'has_wiki', 'archived', 'open_issues', 'license', 'network_count']
 
         reqRepoData = {}
 
@@ -58,6 +58,62 @@ def insertRepo(dbConfig, repoData):
             print("Database {0} doesnt Exist".format(dbConfig['database']))
         else:
             print(err)
+    else:
+        cnx.close()
+
+    repoCommitsData = myghdata.getRepoCommitsInfo(repoData['full_name'])
+    insertRepoCommits(dbConfig, repoCommitsData, repoData['id'])
+
+
+def insertRepoCommits(dbConfig, repoCommitsData, repoId):
+    print("\ndata reached in insert\n")
+    print("Repo id is {}\n".format(repoId))
+
+    try:
+        cnx = mysql.connector.connect(**dbConfig)  # Connection creation
+        print("\nConnection with Database Successful for Repo Commits Table\n")
+        myCursor = cnx.cursor()  # Cursor Creation
+
+        repoCommitsData[0]['repo_id'] = repoId
+        repoCommitsData[0]['comment_count'] = repoCommitsData[0]['commit']['comment_count']
+        repoCommitsData[0]['author_id'] = repoCommitsData[0]['author']['id']
+        repoCommitsData[0]['committer_id'] = repoCommitsData[0]['committer']['id']
+        repoCommitsData[0]['message'] = repoCommitsData[0]['commit']['message']
+
+        ghAttribs = ['sha', 'repo_id', 'comment_count', 'author_id', 'committer_id', 'message']
+
+        reqRepoCommitsData = {}
+
+        for k in repoCommitsData[0]:
+            if k in ghAttribs:
+                reqRepoCommitsData[k] = repoCommitsData[0][k]
+
+        for k in reqRepoCommitsData:
+            if reqRepoCommitsData[k] == '':
+                reqRepoCommitsData[k] = None
+
+        for k, v in reqRepoCommitsData.items():
+            print('{}:{}'.format(k, v))
+
+        insertRepoCommitsQuery = """INSERT INTO commit (sha, repo_id, comment_count, author_id, committer_id, message)
+             VALUES
+             (%(sha)s, %(repo_id)s, %(comment_count)s, %(author_id)s, %(committer_id)s, %(message)s)"""
+
+        myCursor.execute(insertRepoCommitsQuery, reqRepoCommitsData)
+        cnx.commit()
+        myCursor.close()
+
+        print("\nData Inserted Successfully in Repo Commits Table\n")
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Wrong Username of Password for Database Connection")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database {0} doesnt Exist".format(dbConfig['database']))
+        else:
+            print(err)
+            sys.exit(1)  # If data doesnt got inserted in user table, system should
+            # stop here, without automatically inserting data in repo table.
     else:
         cnx.close()
 
