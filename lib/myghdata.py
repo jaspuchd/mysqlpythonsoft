f16192e99acbd9dee8bfd70e07f173cad05d5706
jaswinder Singh <jaswinder.davc@gmail.com>
2018-01-01 12:23:15 +0530

    getRepoCommitsInfo() method added into myghdata.py. But fetching only default 30 commits. Logic to fetch all commits will be added later. All other files unchanged.

--- lib/myghdata.py
+++ lib/myghdata.py
@@ -8,10 +8,21 @@ headers = {'Content-Type': 'application/json',
            'Accept': 'application/vnd.github.v3+json'}
 
 
+def getRepoCommitsInfo(ghFullRepoName):
+    url = '{0}repos/{1}/commits'.format(baseUrl, ghFullRepoName)
+    r = requests.get(url, auth=(username, password), headers=headers)
+    if r.status_code == 200:
+        print("Status code is 200 for repo commits data")
+        return json.loads(r.text)
+    else:
+        return None
+
+
 def getUserInfo(ghUserLogin):
     url = '{0}users/{1}'.format(baseUrl, ghUserLogin)
     r = requests.get(url, auth=(username, password), headers=headers)
     if r.status_code == 200:
+        print("Status code is 200 for user data")
         return json.loads(r.text)
     else:
         return None
@@ -21,7 +32,7 @@ def getRepoInfo(ghFullRepoName):
     url = '{0}repos/{1}'.format(baseUrl, ghFullRepoName)
     r = requests.get(url, auth=(username, password), headers=headers)
     if r.status_code == 200:
-        print("Status code is 200")
+        print("Status code is 200 for repo data")
         return json.loads(r.text)
     else:
         return None
