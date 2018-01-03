eb2f7b7b0d495004da11ff3a6e1893a4df1fece9
Jaswinder Singh <jaswinder.davc@gmail.com>
2018-01-03 16:24:17 +0530

    Only for repo's with multiple pages, also no error checking involved. myghdata.py file modified as getRepoCommitsInfo() method modified for handling pagination. New methods findLastPageNo() and findNextUrl() are added in the myghdata.py file to work on link header to find lastpageno (when default 30 pages OR for per_page=100 is specified in URL) as well as next url. Still only first commit record added to "commit" table as per coding of mydatabase.py file.

--- lib/myghdata.py
+++ lib/myghdata.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import re
 
 baseUrl = 'https://api.github.com/'
 username, password = 'jaspu', 'na1244pata'
@@ -8,14 +9,41 @@ headers = {'Content-Type': 'application/json',
            'Accept': 'application/vnd.github.v3+json'}
 
 
-def getRepoCommitsInfo(ghFullRepoName):
-    url = '{0}repos/{1}/commits'.format(baseUrl, ghFullRepoName)
-    r = requests.get(url, auth=(username, password), headers=headers)
-    if r.status_code == 200:
-        print("Status code is 200 for repo commits data")
-        return json.loads(r.text)
+def findLastPageNo(link):
+    lastPageNo = link.split(',')[1]
+    lastPageNo = re.findall(r'<(.*?)>', lastPageNo)[0]
+    lastPageNo = lastPageNo.split('?')[1]
+    if '&' in lastPageNo:
+        lastPageNo = lastPageNo.split('=')[2]
     else:
-        return None
+        lastPageNo = lastPageNo.split('=')[1]
+    return int(lastPageNo)
+
+
+def findNextUrl(link):
+    nextUrl = link.split(',')[0]
+    nextUrl = re.findall(r'<(.*?)>', nextUrl)[0]
+    return nextUrl
+
+
+def getRepoCommitsInfo(ghFullRepoName):
+
+    url = '{0}repos/{1}/commits?per_page=100'.format(baseUrl, ghFullRepoName)
+    r = requests.get(url, auth=(username, password), headers=headers)
+    repoCommitsData = json.loads(r.text)
+    print(len(repoCommitsData))
+    link = r.headers.get('link', None)
+    nextUrl = findNextUrl(link)
+    lastPageNo = findLastPageNo(link)
+
+    for i in range(2, lastPageNo + 1):
+        r = requests.get(nextUrl, auth=(username, password), headers=headers)
+        repoCommitsData.extend(json.loads(r.text))
+        print("Records Fetched: {}".format(len(repoCommitsData)))
+        link = r.headers.get('link', None)
+        nextUrl = findNextUrl(link)
+
+    return repoCommitsData
 
 
 def getUserInfo(ghUserLogin):
