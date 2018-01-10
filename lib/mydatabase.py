a90f0cde808dfbc61cfdd643d0a76785a5ca9879
Jaswinder Singh <jaswinder.davc@gmail.com>
2018-01-10 09:38:49 +0530

    Implementatoin complete for insertRepoIssues() in mydatabase.py. insertRepo(), insertUser(), insertRepoCommits() modified for datetime issue, now all methods are using datetime.strptime() method for converting date into proper format. No use of STR_TO_DATE method. Job complete for "issues" branch.

--- lib/mydatabase.py
+++ lib/mydatabase.py
@@ -2,6 +2,7 @@ import mysql.connector
 from mysql.connector import errorcode
 from lib import myghdata
 import sys
+import datetime
 # insertRepo method inserts repo data from myfetchdata module into MySql database
 
 
@@ -40,15 +41,15 @@ def insertRepo(dbConfig, repoData):
             if reqRepoData[k] == '':
                 reqRepoData[k] = None
 
-        # for k,v in reqRepoData.items():
-        #     print('{}:{}'.format(k,v))
+        reqRepoData['created_at'] = datetime.datetime.strptime(reqRepoData['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+        reqRepoData['updated_at'] = datetime.datetime.strptime(reqRepoData['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+        reqRepoData['pushed_at'] = datetime.datetime.strptime(reqRepoData['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
 
         insertRepoQuery = """INSERT INTO repo (id, name, full_name, user_id, is_fork, created_at,updated_at,
         pushed_at, homepage, size, stargazers_count, watchers_count, forks, primary_language, has_issues,
         has_pages, has_wiki, is_archived, open_issues, license, network_count)
         VALUES
-        (%(id)s,%(name)s,%(full_name)s,%(user_id)s,%(fork)s,STR_TO_DATE(%(created_at)s, "%Y-%m-%dT%TZ"),
-        STR_TO_DATE(%(updated_at)s, "%Y-%m-%dT%TZ"),STR_TO_DATE(%(pushed_at)s, "%Y-%m-%dT%TZ"),
+        (%(id)s,%(name)s,%(full_name)s,%(user_id)s,%(fork)s,%(created_at)s, %(updated_at)s,%(pushed_at)s,
         %(homepage)s,%(size)s,%(stargazers_count)s,%(subscribers_count)s,%(forks)s,%(language)s,%(has_issues)s,
         %(has_pages)s,%(has_wiki)s,%(archived)s,%(open_issues)s, %(license)s, %(network_count)s)"""
 
@@ -96,7 +97,62 @@ def insertRepo(dbConfig, repoData):
 
 
 def insertRepoIssues(dbConfig, repoIssuesData, repoId):
-    pass
+    try:
+        cnx = mysql.connector.connect(**dbConfig)  # Connection creation
+        print("\nConnection with Database Successful for Repo Issues Table")
+        myCursor = cnx.cursor()  # Cursor Creation
+
+        reqRepoIssuesData = []
+
+        for k in range(0, len(repoIssuesData)):
+            dictForEachIssueRecord = {}
+            dictForEachIssueRecord['id'] = repoIssuesData[k]['id']
+            dictForEachIssueRecord['number'] = repoIssuesData[k]['number']
+            dictForEachIssueRecord['repo_id'] = repoId
+            dictForEachIssueRecord['title'] = repoIssuesData[k]['title']
+            dictForEachIssueRecord['created_by'] = repoIssuesData[k]['user']['id']
+            dictForEachIssueRecord['state'] = repoIssuesData[k]['state']
+            dictForEachIssueRecord['is_locked'] = repoIssuesData[k]['locked']
+            dictForEachIssueRecord['milestone_id'] = repoIssuesData[k]['milestone']['id'] if repoIssuesData[k]['milestone'] else None
+            dictForEachIssueRecord['is_pull_request'] = True if 'pull_request' in repoIssuesData[k] else False
+            dictForEachIssueRecord['comments_count'] = repoIssuesData[k]['comments']
+            dictForEachIssueRecord['created_at'] = datetime.datetime.strptime(repoIssuesData[k]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+            dictForEachIssueRecord['updated_at'] = datetime.datetime.strptime(repoIssuesData[k]['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+
+            if repoIssuesData[k]['closed_at']:
+                dictForEachIssueRecord['closed_at'] = datetime.datetime.strptime(repoIssuesData[k]['closed_at'], "%Y-%m-%dT%H:%M:%SZ")
+            else:
+                dictForEachIssueRecord['closed_at'] = None
+
+            reqRepoIssuesData.append(dictForEachIssueRecord)
+
+        for k in range(0, len(reqRepoIssuesData)):
+            for l in reqRepoIssuesData[k]:
+                if reqRepoIssuesData[k][l] == '':
+                    reqRepoIssuesData[k][l] = None
+
+        insertRepoIssuesQuery = """INSERT INTO issue (id, number, repo_id, title, created_by, state, is_locked, milestone_id, is_pull_request, comments_count, created_at, updated_at, closed_at)
+             VALUES
+             (%(id)s, %(number)s, %(repo_id)s, %(title)s, %(created_by)s, %(state)s, %(is_locked)s, %(milestone_id)s,
+             %(is_pull_request)s, %(comments_count)s, %(created_at)s, %(updated_at)s, %(closed_at)s)"""
+
+        myCursor.executemany(insertRepoIssuesQuery, reqRepoIssuesData)
+        cnx.commit()
+        myCursor.close()
+
+        print("\nData Inserted Successfully in Repo Issues Table\n")
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Wrong Username of Password for Database Connection")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database {0} doesnt Exist".format(dbConfig['database']))
+        else:
+            print(err)
+            sys.exit(1)  # If data doesnt got inserted in issue table, system should
+            # stop here, without automatically inserting data in further tables.
+    else:
+        cnx.close()
 
 
 def insertRepoContents(dbConfig, repoContentsData, repoId):
@@ -121,7 +177,7 @@ def insertRepoContents(dbConfig, repoContentsData, repoId):
         for k in range(0, len(reqRepoContentsData)):
             for l in reqRepoContentsData[k]:
                 if reqRepoContentsData[k][l] == '':
-                    reqRepoContentsData[k] = None
+                    reqRepoContentsData[k][l] = None
 
         insertRepoContentsQuery = """INSERT INTO contents (sha, repo_id, name, path, size, type)
              VALUES
@@ -158,22 +214,21 @@ def insertRepoCommits(dbConfig, repoCommitsData, repoId):
             forEachCommitRecord['comment_count'] = repoCommitsData[k]['commit']['comment_count']
             forEachCommitRecord['author_name'] = repoCommitsData[k]['commit']['author']['name']
             forEachCommitRecord['author_email'] = repoCommitsData[k]['commit']['author']['email']
-            forEachCommitRecord['author_date'] = repoCommitsData[k]['commit']['author']['date']
+            forEachCommitRecord['author_date'] = datetime.datetime.strptime(repoCommitsData[k]['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")
             forEachCommitRecord['committer_name'] = repoCommitsData[k]['commit']['committer']['name']
             forEachCommitRecord['committer_email'] = repoCommitsData[k]['commit']['committer']['email']
-            forEachCommitRecord['committer_date'] = repoCommitsData[k]['commit']['committer']['date']
+            forEachCommitRecord['committer_date'] = datetime.datetime.strptime(repoCommitsData[k]['commit']['committer']['date'], "%Y-%m-%dT%H:%M:%SZ")
             reqRepoCommitsData.append(forEachCommitRecord)
 
         for k in range(0, len(reqRepoCommitsData)):
             for l in reqRepoCommitsData[k]:
                 if reqRepoCommitsData[k][l] == '':
-                    reqRepoCommitsData[k] = None
+                    reqRepoCommitsData[k][l] = None
 
         insertRepoCommitsQuery = """INSERT INTO commit (sha, repo_id, comment_count, author_name, author_email, author_date, committer_name,committer_email,committer_date)
              VALUES
-             (%(sha)s, %(repo_id)s, %(comment_count)s, %(author_name)s,%(author_email)s,
-             STR_TO_DATE(%(author_date)s, "%Y-%m-%dT%TZ"), %(committer_name)s, %(committer_email)s,
-             STR_TO_DATE(%(committer_date)s, "%Y-%m-%dT%TZ"))"""
+             (%(sha)s, %(repo_id)s, %(comment_count)s, %(author_name)s,%(author_email)s, %(author_date)s, %(committer_name)s,
+             %(committer_email)s, %(committer_date)s)"""
 
         myCursor.executemany(insertRepoCommitsQuery, reqRepoCommitsData)
         cnx.commit()
@@ -214,6 +269,9 @@ def insertUser(dbConfig, userData):
             if reqUserData[k] == '':
                 reqUserData[k] = None
 
+        reqUserData['created_at'] = datetime.datetime.strptime(reqUserData['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+        reqUserData['updated_at'] = datetime.datetime.strptime(reqUserData['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+
         # for k,v in reqUserData.items():
         #     print('{}:{}'.format(k,v))
 
@@ -221,8 +279,7 @@ def insertUser(dbConfig, userData):
             public_repos, public_gists, followers, following, created_at, updated_at)
             VALUES
             (%(id)s, %(login)s, %(name)s, %(company)s, %(blog)s, %(location)s, %(email)s, %(hireable)s,
-             %(public_repos)s, %(public_gists)s, %(followers)s, %(following)s,
-             STR_TO_DATE(%(created_at)s, "%Y-%m-%dT%TZ"),STR_TO_DATE(%(updated_at)s, "%Y-%m-%dT%TZ"))"""
+             %(public_repos)s, %(public_gists)s, %(followers)s, %(following)s, %(created_at)s, %(updated_at)s)"""
 
         myCursor.execute(insertUserQuery, reqUserData)
         cnx.commit()
